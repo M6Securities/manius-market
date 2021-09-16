7fb2cf2060d1826ca1f395e55205aed6509c8051
Dalen Ward <dalen.ward@m6securities.com>
2021-09-15 20:04:52 -0600

    created market create form

--- app/controllers/app/market_controller.rb
+++ app/controllers/app/market_controller.rb
@@ -1,4 +1,12 @@
+# frozen_string_literal: true
+
 module App
+  # Handles user facing market stuff
   class MarketController < AppController
+
+    def create
+      safe_params = params.require(:create).permit(:display_name, :path_name, :email)
+    end
+
   end
 end
