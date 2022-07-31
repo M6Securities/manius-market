8ae57d3df27bc81d620e471cf87b01dea77e4e80
Dalen Ward <dalen.ward@m6securities.com>
2022-07-31 15:52:25 -0600

    greatly improved cart functionality

--- app/javascript/controllers/site/cart/item_controller.js
+++ app/javascript/controllers/site/cart/item_controller.js
@@ -9,9 +9,21 @@ export default class extends Controller {
     $('.touchspin-cart').TouchSpin();
     updateFeather();
 
-    this.quantityTarget.addEventListener('change', function (_event) {
-      $('#navbar-cart-update-div').show();
+    const that = this;
+
+    this.quantityTarget.addEventListener('change', function (event) {
+      that.changeEvent(event);
     });
+
+    $('.cart-item-qty button.bootstrap-touchspin-up').on('click', function (event) {
+      that.changeEvent(event);
+    });
+
+    $('.cart-item-qty button.bootstrap-touchspin-down').on('click', function (event) {
+      that.changeEvent(event);
+    });
+
+    this.updateCartCount();
   }
 
   removeItem() {
@@ -20,6 +32,37 @@ export default class extends Controller {
     element.remove();
     $('#navbar-cart-update-div').show();
 
-    console.log("removed item");
+    // console.log("removed item");
+  }
+
+  changeEvent(event) {
+    $('#navbar-cart-update-div').show();
+    this.updateCartCount();
+  }
+
+  updateCartCount() {
+    // calculate the number of items in the cart, and update the navbar
+    const itemInputs = $('.cart-navbar-input');
+    let numItems = 0;
+    // for every element in itemInputs, add the value to numItems
+    itemInputs.each(function () {
+      const value = parseInt($(this).val());
+
+      //if value is not NaN, add it to numItems
+      if (isNaN(value)) {
+          numItems += 0;
+      } else {
+          numItems += value;
+      }
+    });
+    const itemCountElement = $('#cart-item-count');
+
+    // if it exists
+    if (itemCountElement.length > 0) {
+      itemCountElement.text(numItems);
+    } else {
+      // if it doesn't exist, create it
+      $('#cart-navbar-dropdown-button').append(`<span class="badge rounded-pill bg-primary badge-up" id="cart-item-count">${numItems}</span>`);
+    }
   }
 }
